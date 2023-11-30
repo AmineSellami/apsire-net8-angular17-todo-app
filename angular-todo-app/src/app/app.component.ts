dd94fb48f78eddfe2836a5b4d502b48448df496a
Mohamed Amine Sellami <mohamedamine.sellami@outlook.com>
2023-11-30 22:19:16 +0000

    fix template

--- angular-todo-app/src/app/app.component.ts
+++ angular-todo-app/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 
@@ -13,10 +14,13 @@ import { MatDividerModule } from '@angular/material/divider';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, MatCardModule, MatInputModule,
-  MatButtonModule, MatIconModule, MatDividerModule],
+  MatButtonModule, MatIconModule, MatDividerModule, MatCheckboxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  //todos : {} = [] ;
+  todos = [{ id: 1, title: 'First Todo', isDone: false, createdAt: new Date() },
+  { id: 2, title: 'Second Todo', isDone: true, createdAt: new Date() },
+  { id: 3, title: 'Third Todo', isDone: false, createdAt: new Date() }];
 }
